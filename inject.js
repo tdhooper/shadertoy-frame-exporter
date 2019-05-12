498df87b7b28e854062a2e5e1de740f0374b02c7
Thomas Hooper <tom@stainlessvision.com>
2019-05-12 01:16:04 +0100

    Update to FileSaver 2.0.0

--- inject.js
+++ inject.js
@@ -6,4 +6,4 @@ function injectScript(file, node) {
     th.appendChild(s);
 }
 injectScript( chrome.extension.getURL('/main.js'), 'body');
-injectScript( chrome.extension.getURL('/lib/FileSaver-1.3.3.min.js'), 'body');
+injectScript( chrome.extension.getURL('/lib/FileSaver-2.0.0.min.js'), 'body');
