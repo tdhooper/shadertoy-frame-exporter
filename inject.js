3f71ee0032c3f131a248f65e6661fe21ca7d794d
tdhooper <projects@stainlessvision.com>
2024-12-03 23:41:19 +0000

    Migrate to Manifest V3

--- inject.js
+++
@@ -1,9 +0,0 @@
-function injectScript(file, node) {
-    var th = document.getElementsByTagName(node)[0];
-    var s = document.createElement('script');
-    s.setAttribute('type', 'text/javascript');
-    s.setAttribute('src', file);
-    th.appendChild(s);
-}
-injectScript( chrome.extension.getURL('/main.js'), 'body');
-injectScript( chrome.extension.getURL('/lib/FileSaver-2.0.4.min.js'), 'body');
